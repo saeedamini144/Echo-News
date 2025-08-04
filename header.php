77e6224d8b30457781b6555d14fff7fbb836cf59
saeed <saaed.amini@gmail.com>
2025-08-04 09:05:26 +0330

    delet the unused css from the main page

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" data-theme="light" wp-site wp-site-is-master-page wp-site-theme-name="echo news" wp-site-theme-slug="echonews" wp-site-themes-folder="C:\Users\ak\Local Sites\vespern\app\public\wp-content\themes\echo news">
+<html lang="en" data-theme="light">
 
 <head>
     <meta charset="UTF-8">
@@ -280,13 +280,13 @@
                                         <a href="#"><i class="fa-brands fa-youtube"></i></a>
                                     </li>
                                 </ul>
-                                <div class="echo-header-top-menu-bar menu-btn">
+                                <!-- <div class="echo-header-top-menu-bar menu-btn">
                                     <a href="javascript:void(0)">
                                         <svg width="20" height="19" viewbox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M0.526001 0.953461H20V3.11724H0.526001V0.953461ZM7.01733 8.52668H20V10.6905H7.01733V8.52668ZM0.526001 16.0999H20V18.2637H0.526001V16.0999Z" fill="#5E5E5E" />
                                         </svg>
                                     </a>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
                     </div>
