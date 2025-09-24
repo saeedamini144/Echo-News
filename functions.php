f404eef33527825aa46e5ee3e19e850b574c28ad
saeed <saaed.amini@gmail.com>
2025-09-24 12:01:55 +0330

    start make the customize for the header

--- functions.php
+++ functions.php
@@ -21,6 +21,7 @@ function register_incFile()
     require_once get_template_directory() . '/inc/plugins.php';
     require_once get_template_directory() . '/inc/class-custom-menu-walker.php';
     require_once get_template_directory() . '/inc/customizer.php';
+    require_once get_template_directory() . '/inc/post-callback-functions.php';
 }
 add_action('after_setup_theme', 'register_incFile'); //note load the correct style in dashboard at least fix it up
 
--- inc/customizer.php
+++ inc/customizer.php
@@ -1,43 +1,63 @@
- <?
-    if (class_exists('Kirki')) {
+<?php
 
-        // کانفیگ قالب
-        Kirki::add_config('my_theme_config', [
-            'capability' => 'edit_theme_options',
-            'option_type' => 'theme_mod',
+require_once get_template_directory() . '/inc/post-callback-functions.php';
 
-        ]);
+if (class_exists('Kirki')) {
 
-        // پنل
-        Kirki::add_panel('theme_options', [
-            'title' => esc_html__('تنظیمات قالب', 'textdomain'),
-            'priority' => 10,
-            'description' => esc_html__('تنظیمات مختلف قالب را از اینجا انجام دهید.', 'textdomain'),
-        ]);
+    // Config
+    Kirki::add_config('EchoNews_Theme_Options_Config', [
+        'capability' => 'edit_theme_options',
+        'option_type' => 'theme_mod',
+    ]);
 
-        // سکشن رنگ‌ها
-        Kirki::add_section('colors_section', [
-            'title' => esc_html__('رنگ‌ها', 'textdomain'),
-            'panel' => 'theme_options',
-            // 'priority' => 10,
-            'description' => esc_html__('تنظیم رنگ‌های قالب از اینجا.', 'textdomain'),
-        ]);
+    //main panel
+    Kirki::add_panel('EchoNews_Theme_Options_Panel', [
+        'title' => esc_html__('Theme Options', 'textdomain'),
+        'description' => esc_html__('Manage your Theme options from here.', 'textdomain'),
+        'priority' => 10,
+    ]);
 
-        // فیلد رنگ هدر
-        Kirki::add_field('my_theme_config', [
-            'type' => 'color',
-            'settings' => 'header_bg_color',
-            'label' => esc_html__('رنگ پس‌زمینه هدر', 'textdomain'),
-            'section' => 'colors_section',
-            'default' => '#ffffff',
-            'transport' => 'auto',
-        ]);
-    } else {
-        function EchoNews_notice_kirki_missing()
-        {
-            echo '<div class="error"><p>';
-            echo esc_html__('Please for changing theme options install and acivate the kiki plugin.', 'textdomain');
-            echo '</p></div>';
-        }
-        add_action('admin_notices', 'EchoNews_notice_kirki_missing');
+    //header options
+    Kirki::add_section('header_options', [
+        'title' => esc_html__('Header Options', 'textdomain'),
+        'description' => esc_html__('You can change the header options', 'textdomain'),
+        'panel' => 'EchoNews_Theme_Options_Panel',
+    ]);
+
+    // header background color field
+    // Kirki::add_field('EchoNews_Theme_Options_Config', [
+    //     'type' => 'color',
+    //     'settings' => 'header_color',
+    //     'label' => esc_html__('Header Color', 'textdomain'),
+    //     'section' => 'header_options',
+    //     'default' => '#ffffff',
+    //     'transport' => 'auto',
+    //     'output' => [
+    //         [
+    //             'element' => '.echo-header-top',
+    //             'property' => 'background-color',
+    //         ],
+    //     ]
+    // ]);
+
+    //vertical header slider
+    kirki::add_field('vertical_header_post_slider', [
+        'label' => esc_html__('Choose Category', 'textdomain'),
+        'description' => esc_html__('Select the category for vertical header post slider', 'textdomain'),
+        'section' => 'header_options',
+        'transport' => 'auto',
+        // 'default'=>'',
+        'type' => 'select',
+        'multiple'    => true,
+        'settings' => 'vertical_header_post_slider',
+        'choices' =>  EchoNews_get_all_category(),
+    ]);
+} else {
+    function EchoNews_notice_kirki_missing()
+    {
+        echo '<div class="error"><p>';
+        echo esc_html__('Please install and activate the Kirki plugin to change theme options.', 'textdomain');
+        echo '</p></div>';
     }
+    add_action('admin_notices', 'EchoNews_notice_kirki_missing');
+}
