132c3f62342eb31a36e6dec40145ed52d9bd297f
saeed <saaed.amini@gmail.com>
2025-10-01 16:48:51 +0330

    add the search dynamice

--- searchform.php
+++ searchform.php
@@ -1,4 +1,3 @@
  <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
      <input id="searchInput1" class="search-input" type="search" name="s" value="<?php echo get_search_query(); ?>" placeholder="Search ...">
  </form>
- <!-- //test git -->
